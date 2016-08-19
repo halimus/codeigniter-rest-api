be37ec9c02fb6063fe35bca55b42676f7d34f7c4
LARDJANE <halim.lardjane@gmail.com>
2016-08-19 17:38:21 -0400

    Update the code

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Api';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['author/(:any)'] = 'author/author';
